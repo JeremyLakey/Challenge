24e26f2b4a0e808d1c1555e898dc8e3885fe8f0f
personman61 <jeremysl123@gmail.com>
2022-07-09 23:54:50 -0600

    Finished Linked list items
    Finished the get, add, reset, next functions

---
+++ C++ Linked List/Link List/LinkList/LinkedList.h
@@ -0,0 +1,69 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+
+template <class T>
+class LinkedList {
+public:
+	LinkedList();
+
+	void add(T value);
+
+	T get();
+
+	void next();
+
+	void reset();
+
+private:
+	struct node {
+		T item;
+		node* next;
+	};
+
+	node* head;
+	node* current;
+
+	
+};
+
+template <class T>
+LinkedList<T>::LinkedList() {
+	this->head = 0;
+	this->current = 0;
+}
+
+template <class T>
+void LinkedList<T>::add(T value) {
+	node* adding = new node();
+	adding->item = value;
+	adding->next = 0;
+	node* temp = this->head;
+	if (temp == 0x00000000) {
+		this->head = adding;
+		this->current = adding;
+		return;
+	}
+	while (temp->next != 0) temp = temp->next;
+	temp->next = adding;
+}
+
+template <class T>
+void LinkedList<T>::reset() {
+	this->current = this->head;
+}
+
+template <class T>
+void LinkedList<T>::next() {
+	this->current = this->current->next;
+}
+
+template <class T>
+T LinkedList<T>::get() {
+	return this->current->item;
+}
+
+
+
+
+#endif
--- C++ Linked List/Link List/LinkList/main.cpp
+++ C++ Linked List/Link List/LinkList/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
-
+#include <string> 
+#include "LinkedList.h"
 using std::cout;
+using std::to_string;
 
 int main(int argc, char** argv) {
-	cout << "love you\n";
+	LinkedList<int> intList;
+	intList.add(0);
+	intList.add(1);
+	intList.add(3);
+
+	cout << to_string(intList.get()) + "\n";
+	intList.next();
+	cout << to_string(intList.get()) + "\n";
+	intList.next();
+	cout << to_string(intList.get()) + "\n";
+	intList.reset();
+	cout << to_string(intList.get()) + "\n";
+	intList.next();
+	cout << to_string(intList.get()) + "\n";
+	intList.next();
+	cout << to_string(intList.get()) + "\n";
 	return 0;
 }
